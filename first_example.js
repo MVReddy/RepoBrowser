fe61bdc6aea415090918eb88b6898f8b359b70b3
Venkata Reddy M <venkatareddy.mulam@techmahindra.com>
2018-05-06 09:00:00 -0400

    Update first_example.js

--- first_example.js
+++ first_example.js
@@ -12,3 +12,14 @@ http.createServer(function (req, res) {
 	res.write(txt)
     res.end();
 }).listen(8080);
+
+http.createServer_new(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("The date and time are currently: " + dt.myDateTime());
+	//res.write(req.url);
+	
+	var q = url.parse(req.url, true).query;
+	var txt = q.year + " " + q.month;
+	res.write(txt)
+    res.end();
+}).listen(8081);
